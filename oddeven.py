e8f7209c7a7e8950efc04546461922fd300dc630
cyber-holmes <67431536+cyber-holmes@users.noreply.github.com>
2020-12-25 17:29:00 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,10 @@
+#Goal:Check whether the given input is an odd or even number
+
+#Step1:Take user input in integer value
+a = input("Enter the number:")
+
+#Step2:Use Conditional statement's  
+if a %2 == 0:
+ print("The number given is even")
+else :
+ print ("The number given is odd")
